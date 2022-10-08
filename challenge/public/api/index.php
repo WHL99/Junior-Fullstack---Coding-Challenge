2cac1e7ae85e04a8c3aac9548cf6b9be2b13f9b8
WHL99 <xjv298347@gmail.com>
2022-10-08 16:33:23 +0200

    Fetch start reading time (cookie) from backend to frontend, which replace 'startReadTime'. Create API endpoint '/reading-result'.

--- challenge/public/api/index.php
+++ challenge/public/api/index.php
@@ -19,29 +19,19 @@ $app->setBasePath('/api');
 $app->addRoutingMiddleware();
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
 
-/**
- * TODO
- * Here you can write your own API endpoints.
- * You can use Redis and/or cookies for data persistence.
- *
- * Find below an example of a GET endpoint that uses redis to temporarily store a name,
- * and cookies to keep track of an event date and time.
- */
-
-$app->get('/hello/{name}', function (Request $request, Response $response, $args) {
+$app->get('/reading-result', function (Request $request, Response $response, $args) {
 
     // Redis usage example:
     /** @var \Predis\Client $redisClient */
-    $redisClient = $this->get('redisClient');
-    $oldName = $redisClient->get('name');
-    if (is_string($oldName)) {
-        $name = $oldName;
-    } else {
-        $redisClient->set('name', $args['name'], 'EX', 10);
-        $name = $args['name'];
-    }
+    // $redisClient = $this->get('redisClient');
+    // $oldName = $redisClient->get('name');
+    // if (is_string($oldName)) {
+    //     $name = $oldName;
+    // } else {
+    //     $redisClient->set('name', $args['name'], 'EX', 10);
+    //     $name = $args['name'];
+    // }
 
-    // Setting a cookie example:
     $cookieValue = '';
     if (empty($_COOKIE["FirstSalutationTime"])) {
         $cookieName = "FirstSalutationTime";
@@ -50,10 +40,7 @@ $app->get('/hello/{name}', function (Request $request, Response $response, $args
         setcookie($cookieName, $cookieValue, $expires, '/');
     }
 
-    // Response example:
     $response->getBody()->write(json_encode([
-        'name' => $name,
-        'salutation' => "Hello, $name!",
         'first_salutation_time' => $_COOKIE["FirstSalutationTime"] ?? $cookieValue,
     ], JSON_THROW_ON_ERROR));
 
